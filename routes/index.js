ea4b23006422beed42b5db02ae73af5732024776
Paul Laborderie <paul.laborderie@gmail.com>
2021-01-09 00:52:37 +0100

    Adding sample messages and styling

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+// Sample messages
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini message board', messages });
 });
 
 module.exports = router;
